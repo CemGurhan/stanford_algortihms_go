8950cf23e7d03a9df1d79b13ed34f428159f5101
CemGurhan <cemg@live.co.uk>
2023-03-28 08:46:13 +0100

    add brute force number of inversions calculator

---
+++ algorithms/inversions/brute_force.go
@@ -0,0 +1,20 @@
+package inversions
+
+import "fmt"
+
+func Number_of_inversions(array []int) {
+	var number_of_inversions [][]int
+	k := 0
+	for i := 0; i < len(array); i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] > array[j] {
+				number_of_inversions = append(number_of_inversions, []int{array[i], array[j]})
+				k++
+			}
+		}
+	}
+	fmt.Println("Inverstions: ", number_of_inversions)
+}
+
+// [2, 3, 7, 8, 9, 1]
+// [[2,1], [3,1], [7,1], ]
--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 
 	"github.com/cemgurhan/algorithms/algorithms"
+	"github.com/cemgurhan/algorithms/algorithms/inversions"
 )
 
 func main() {
+	array := []int{2, 3, 7, 8, 9, 1}
+	inversions.Number_of_inversions(array)
+}
+
+func call_merge_sort() {
 	array := []int{22, 4, -1, 3, 55}
 	sorted_array := algorithms.Merge_sort(array)
 	fmt.Println(sorted_array)
