7356b786a02bcc6dfcac03f946e3fad8e876c7ad
CemGurhan <cemg@live.co.uk>
2023-04-25 09:51:31 +0100

    implement brute force closest pair

--- algorithms/closest_pair/one_dimension/brute_force.go
+++ algorithms/closest_pair/one_dimension/brute_force.go
@@ -1,27 +1,83 @@
 package onedimension
 
-func FindClosestPairBrute(oneDimensionalPlane []int) int {
-	distanceBetweenPoints := -1
-	pairCounter := 0
+func FindClosestPairBrute(oneDimensionalPlane []int) (int, []int) {
+	distanceBetweenPointsOne := 0
+	distanceBetweenPointsTwo := 0
+	closestDistanceBetweenPoints := 0
+	closestPointPairOne := make([]int, 2)
+	closestPointPairTwo := make([]int, 2)
+	finalClosestPair := make([]int, 2)
+	pairFound := false
+	iterations := 0
 
 	for i := 0; i < len(oneDimensionalPlane); i++ {
+		if i+1 == len(oneDimensionalPlane) && distanceBetweenPointsTwo == 0 {
+			closestDistanceBetweenPoints = distanceBetweenPointsOne
+			finalClosestPair = closestPointPairOne
+			break
+		}
+
+		if distanceBetweenPointsOne < distanceBetweenPointsTwo && distanceBetweenPointsOne != 0 {
+			closestDistanceBetweenPoints = distanceBetweenPointsOne
+			finalClosestPair = closestPointPairOne
+		} else if distanceBetweenPointsTwo < distanceBetweenPointsOne && distanceBetweenPointsTwo != 0 {
+			closestDistanceBetweenPoints = distanceBetweenPointsTwo
+			finalClosestPair = closestPointPairTwo
+			distanceBetweenPointsOne = distanceBetweenPointsTwo
+			closestPointPairOne = []int{closestPointPairTwo[0], closestPointPairTwo[1]}
+			distanceBetweenPointsTwo = 0
+		}
+
 		if oneDimensionalPlane[i] == 0 {
 			continue
-		} else if oneDimensionalPlane[i] == 1 {
-			pairCounter++
-			distanceBetweenPoints++
 		}
 
-		if pairCounter == 1 {
-			distanceBetweenPoints++
-		}
+		iterations = 0
 
-		if pairCounter == 2 {
-			pairCounter = 0
+		for j := i + 1; j < len(oneDimensionalPlane); j++ {
+			if oneDimensionalPlane[j] == 1 {
+				if !pairFound {
+					closestPointPairOne[1] = j
+					if distanceBetweenPointsOne == 0 {
+						closestDistanceBetweenPoints = 0
+						finalClosestPair = []int{j - 1, j}
+						closestPointPairOne = []int{j - 1, j}
+						distanceBetweenPointsOne++
+						break
+					}
+				} else {
+					closestPointPairTwo[1] = j
+					if distanceBetweenPointsTwo == 0 {
+						closestDistanceBetweenPoints = 0
+						distanceBetweenPointsOne = distanceBetweenPointsTwo
+						finalClosestPair = []int{j - 1, j}
+						closestPointPairOne = []int{j - 1, j}
+						break
+					}
+				}
+
+				pairFound = true
+				break
+			}
+			if !pairFound {
+				distanceBetweenPointsOne++
+				if iterations == 0 {
+					closestPointPairOne[0] = j - 1
+					iterations++
+				}
+			} else {
+				distanceBetweenPointsTwo++
+				if iterations == 0 {
+					closestPointPairTwo[0] = j - 1
+					iterations++
+				}
+			}
 		}
 	}
 
-	return distanceBetweenPoints
+	return closestDistanceBetweenPoints, finalClosestPair
 }
 
 // [0 0 0 1 0 0 1 0 1 0 0 0 0 1 0 0 1]
+// [0 0 1 0 1 0 0 0 0 1 0 0 1]
+// [1 0 1 1 0 0 1]
--- algorithms/closest_pair/one_dimension/brute_force_test.go
+++ algorithms/closest_pair/one_dimension/brute_force_test.go
@@ -3,13 +3,54 @@ package onedimension
 import (
 	"testing"
 
-	"github.com/stretchy/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFindClosestPairBrute(t *testing.T) {
+func TestFindClosestPairBrute_One(t *testing.T) {
 	oneDimensionalPlane := []int{0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1}
 
-	expectedDistanceBetweenPoints := FindClosestPairBrute(oneDimensionalPlane)
+	actualDistanceBetweenPoints, actualPair := FindClosestPairBrute(oneDimensionalPlane)
 
-	assert.Equal(expectedDistanceBetweenPoints, 2)
+	assert.Equal(t, 1, actualDistanceBetweenPoints)
+	assert.Equal(t, []int{6, 8}, actualPair)
+
+}
+
+func TestFindClosestPairBrute_Two(t *testing.T) {
+	oneDimensionalPlane := []int{0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1}
+
+	actualDistanceBetweenPoints, actualPair := FindClosestPairBrute(oneDimensionalPlane)
+
+	assert.Equal(t, 2, actualDistanceBetweenPoints)
+	assert.Equal(t, []int{2, 5}, actualPair)
+
+}
+
+func TestFindClosestPairBrute_Three(t *testing.T) {
+	oneDimensionalPlane := []int{1, 0, 1, 1, 0, 0, 1}
+
+	actualDistanceBetweenPoints, actualPair := FindClosestPairBrute(oneDimensionalPlane)
+
+	assert.Equal(t, 0, actualDistanceBetweenPoints)
+	assert.Equal(t, []int{2, 3}, actualPair)
+
+}
+
+func TestFindClosestPairBrute_Four(t *testing.T) {
+	oneDimensionalPlane := []int{1, 1, 0, 1, 0, 0, 1}
+
+	actualDistanceBetweenPoints, actualPair := FindClosestPairBrute(oneDimensionalPlane)
+
+	assert.Equal(t, 0, actualDistanceBetweenPoints)
+	assert.Equal(t, []int{0, 1}, actualPair)
+
+}
+
+func TestFindClosestPairBrute_Five(t *testing.T) {
+	oneDimensionalPlane := []int{1, 0, 0, 0, 0, 0, 1}
+
+	actualDistanceBetweenPoints, actualPair := FindClosestPairBrute(oneDimensionalPlane)
+
+	assert.Equal(t, 5, actualDistanceBetweenPoints)
+	assert.Equal(t, []int{0, 6}, actualPair)
 }
